f9d3fd990c73a0fa893717a803baeb2ffcde91d3
yeshwanth0009 <2400030982@kluniversity.in>
2025-09-08 12:36:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const a= "yeshwanth"
+const b= "30982"
+const c= a+b
+document.writeln("Output is c:",c)
+
+const d = 6
+const e = 7
+const f = "mukash"
+const g = d+e+f
+document.writeln("Output is: " ,g)
+
+let h = 10;
+let i = 20;
+let j = h**3;
+let k = i**2;
+document.writeln("Output is: " ,h)
+document.writeln("Output is: " ,i)
+document.writeln("Output is: " ,j)
+document.writeln("Output is: " ,k)
